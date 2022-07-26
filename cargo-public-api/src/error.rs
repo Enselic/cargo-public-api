17d6677dc81caa767d22636f050661bb8cfc4186
Matthias Beyer <mail@beyermatthias.de>
2022-07-26 10:10:49 +0200

    Add error variants for different deny strategies
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- cargo-public-api/src/error.rs
+++ cargo-public-api/src/error.rs
@@ -2,4 +2,13 @@
 pub enum Error {
     #[error("The API diff is not allowed as per --deny")]
     DiffDenied,
+
+    #[error("The API diff is not allowed as per --deny=additions")]
+    DiffAddedDenied,
+
+    #[error("The API diff is not allowed as per --deny=changes")]
+    DiffChangedDenied,
+
+    #[error("The API diff is not allowed as per --deny=deletions")]
+    DiffDeletionsDenied,
 }
